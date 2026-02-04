1c588d45eb61ed047b8071b3c5358243da19e4ab
Nirnay Roy <nroy1012@gmail.com>
2026-02-04 19:37:30 +0530

    max min scalar impl

--- dev/nx-oxcaml/lib/binary_ops/op_min.ml
+++ dev/nx-oxcaml/lib/binary_ops/op_min.ml
@@ -319,7 +319,6 @@ let min_int32 a_arr b_arr out_arr va vb vout start_idx end_idx =
       let b_val = Array.unsafe_get b_arr (b_offset + b_lin) in
       Array.unsafe_set out_arr (out_offset + k) (Int32_u.min a_val b_val)
     done
-(* 
 let min_int64 a_arr b_arr out_arr va vb vout start_idx end_idx =
   let out_base = View.offset vout + start_idx in
   let a_base = View.offset va + start_idx in
@@ -330,30 +329,25 @@ let min_int64 a_arr b_arr out_arr va vb vout start_idx end_idx =
   then (
     let i = ref 0 in
     let n = end_idx - start_idx in
-    let n8 = n - 7 in
-    while !i < n8 do
-      let idx = !i in
-      let a0 = Int64x2.Array.unsafe_get a_arr ~idx:(a_base + idx) in
-      let b0 = Int64x2.Array.unsafe_get b_arr ~idx:(b_base + idx) in
-      let a1 = Int64x2.Array.unsafe_get a_arr ~idx:(a_base + idx + 2) in
-      let b1 = Int64x2.Array.unsafe_get b_arr ~idx:(b_base + idx + 2) in
-      let a2 = Int64x2.Array.unsafe_get a_arr ~idx:(a_base + idx + 4) in
-      let b2 = Int64x2.Array.unsafe_get b_arr ~idx:(b_base + idx + 4) in
-      let a3 = Int64x2.Array.unsafe_get a_arr ~idx:(a_base + idx + 6) in
-      let b3 = Int64x2.Array.unsafe_get b_arr ~idx:(b_base + idx + 6) in
-      Int64x2.Array.unsafe_set out_arr ~idx:(out_base + idx) (Int64x2.min a0 b0);
-      Int64x2.Array.unsafe_set out_arr ~idx:(out_base + idx + 2) (Int64x2.min a1 b1);
-      Int64x2.Array.unsafe_set out_arr ~idx:(out_base + idx + 4) (Int64x2.min a2 b2);
-      Int64x2.Array.unsafe_set out_arr ~idx:(out_base + idx + 6) (Int64x2.min a3 b3);
-      i := idx + 8
-    done;
-    let n2 = n - 1 in
-    while !i < n2 do
-      let idx = !i in
-      let a_vec = Int64x2.Array.unsafe_get a_arr ~idx:(a_base + idx) in
-      let b_vec = Int64x2.Array.unsafe_get b_arr ~idx:(b_base + idx) in
-      Int64x2.Array.unsafe_set out_arr ~idx:(out_base + idx) (Int64x2.min a_vec b_vec);
-      i := idx + 2
+    let n4 = n - 3 in
+    while !i < n4 do
+      let i0 = !i in
+      let i1 = i0 + 1 in
+      let i2 = i0 + 2 in
+      let i3 = i0 + 3 in
+      let a0 = Array.unsafe_get a_arr (a_base + i0) in
+      let b0 = Array.unsafe_get b_arr (b_base + i0) in
+      let a1 = Array.unsafe_get a_arr (a_base + i1) in
+      let b1 = Array.unsafe_get b_arr (b_base + i1) in
+      let a2 = Array.unsafe_get a_arr (a_base + i2) in
+      let b2 = Array.unsafe_get b_arr (b_base + i2) in
+      let a3 = Array.unsafe_get a_arr (a_base + i3) in
+      let b3 = Array.unsafe_get b_arr (b_base + i3) in
+      Array.unsafe_set out_arr (out_base + i0) (Int64_u.min a0 b0);
+      Array.unsafe_set out_arr (out_base + i1) (Int64_u.min a1 b1);
+      Array.unsafe_set out_arr (out_base + i2) (Int64_u.min a2 b2);
+      Array.unsafe_set out_arr (out_base + i3) (Int64_u.min a3 b3);
+      i := i0 + 4
     done;
     while !i < n do
       let idx = !i in
@@ -383,4 +377,4 @@ let min_int64 a_arr b_arr out_arr va vb vout start_idx end_idx =
       let a_val = Array.unsafe_get a_arr (a_offset + a_lin) in
       let b_val = Array.unsafe_get b_arr (b_offset + b_lin) in
       Array.unsafe_set out_arr (out_offset + k) (Int64_u.min a_val b_val)
-    done *)
+    done
